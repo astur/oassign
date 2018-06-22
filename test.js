55fd6f57981cadc72b471076f1e1bc5bdaf0f8a7
astur <astur@yandex.ru>
2018-06-22 11:00:42 +0300

    first test

--- test.js
+++ test.js
@@ -2,6 +2,14 @@ const test = require('ava');
 const m = require('.');
 
 test('oassign', t => {
-    t.true(true);
-    t.is(m, m);
+    t.deepEqual(m(), {});
+    t.deepEqual(m('bad'), {});
+    t.deepEqual(m(null, 'bad', 123), {});
+    t.deepEqual(m({}), {});
+    t.deepEqual(m({}, {}, null, 'bad', 123), {});
+    t.deepEqual(m([1, 2], [3]), {0: 3, 1: 2});
+    t.deepEqual(m({a: 1}), {a: 1});
+    t.deepEqual(m({a: 1, b: 2}, {a: 3, c: 4}), {a: 3, b: 2, c: 4});
+    t.deepEqual(m({}, {a: 1}), {a: 1});
+    t.deepEqual(m({a: 1}, [2], 'bad', null), {a: 1, 0: 2});
 });
