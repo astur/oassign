2d1b37e4af53735fe39fe73c2038989e8dfadb5c
astur <astur@yandex.ru>
2018-06-22 11:00:50 +0300

    first code

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-module.exports = () => {};
+module.exports = (...objs) => {
+    objs = objs.filter(o => ['Object', 'Array'].includes({}.toString.call(o).slice(8, -1)));
+    return Object.assign({}, ...objs);
+};
